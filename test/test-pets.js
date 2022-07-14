5d3346d6ed29fa0884caf6c8624e86b6e6b6f660
Pao <pzsigua@gmail.com>
2022-07-13 18:22:00 -0700

    Implemented responding to JSON

--- test/test-pets.js
+++ test/test-pets.js
@@ -116,4 +116,16 @@ describe('Pets', ()  => {
       });
     });
   });
+
+  it('should list ALL pets on /pets GET', function(done) {
+    chai.request(server)
+        .get('/')
+        .set('content-type', 'application/json')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          done();
+        });
+  });
 });
